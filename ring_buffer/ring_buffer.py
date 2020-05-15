f5deedec2eab283364087066badfb5b4487e18a6
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-05-15 13:16:55 -0400

    finsihed my ring buffer file

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, value, capacity=6):
+        self.head = value
+        self.tail = value
+        self.full = capacity
+        self.length = capacity if node is not None
 
     def append(self, item):
-        pass
+        # current node
+        current = self.head
+        new = item
+
+        if new:
+            new = self.head
+
+        return self.full
 
     def get(self):
-        pass
+        all_values = self.full
+
+        if all_values:
+
+        return self.full
